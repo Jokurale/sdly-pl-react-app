b15dcfad698e0000e4615c4769917f968703e3b1
= <podsiadly.michal@protonmail.com>
2021-02-20 16:11:42 +0100

    Added left jumbotron site

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,9 +1,11 @@
 import Nav from './nav/Nav'
+import Jumbotron from './jumbotron/Jumbotron'
 
 export default function Page() {
     return (
-        <div className="container">
+        <div className="container h-100">
             <Nav />
+            <Jumbotron/>
         </div>
     )
 }
---
+++ src/components/jumbotron/Jumbotron.jsx
@@ -0,0 +1,17 @@
+export default function Jumbotron(props) {
+    return (
+        <div className="container-fluid jumbotron">
+            <div className="row">
+                <div className="col-12">
+                    <div className="jumbo-left col-md-6">
+                        <h1>Michał Podsiadły</h1>
+                        <h4>JUNIOR FULL-STACK DEVELOPER</h4>
+                        <button className="jumbo-left-btn">About me</button>
+                    </div>
+                    <div className="jumbo-right col-md-6">
+                    </div>
+                </div>
+            </div>
+        </div>
+    )    
+}
